a4eed9125d5efbc865c890822d34dc8c097d2964
szabot1 <gleams-aquifer.0d@icloud.com>
2023-12-13 12:46:22 +0100

    add

---
+++ src/instrument-delete.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+
+export default function InstrumentDeletePage() {
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  const [isFetching, setIsFetching] = useState(true);
+  const [instrument, setInstrument] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch("https://kodbazis.hu/api/instruments/" + id, {
+          credentials: "include",
+        });
+        const data = await res.json();
+        setInstrument(data);
+      } finally {
+        setIsFetching(false);
+      }
+    })();
+  }, [id]);
+
+  return (
+    <div className="p-5 m-auto text-center content bg-ivory">
+      {isFetching && <div className="spinner-border"></div>}
+      {!isFetching && !instrument && <p>Hiba történt!</p>}
+      {!isFetching && instrument && (
+        <>
+          <h2 className="mb-4">Hangszer törlése</h2>
+
+          <div className="card mb-4">
+            <form
+              onSubmit={(event) => {
+                event.persist();
+                event.preventDefault();
+
+                fetch("https://kodbazis.hu/api/instruments/" + id, {
+                  method: "DELETE",
+                  credentials: "include",
+                }).then(() => {
+                  navigate("/");
+                });
+              }}
+            >
+              <img
+                src={
+                  instrument.imageURL ??
+                  "https://via.placeholder.com/400x800.png?text=No+image"
+                }
+                alt={instrument.name}
+                style={{
+                  maxHeight: "200px",
+                  objectFit: "contain",
+                }}
+                className="card-img-top mt-4 img-fluid"
+              />
+              <div className="card-body">
+                <p className="card-text text-muted">{instrument.brand}</p>
+                <h5 className="card-title">{instrument.name}</h5>
+                <p className="card-text text-muted">
+                  {new Intl.NumberFormat().format(instrument.price)} Ft -{" "}
+                  {new Intl.NumberFormat().format(instrument.quantity)} db
+                  készleten
+                </p>
+              </div>
+
+              <div
+                className="mt-2 mb-2"
+                style={{
+                  display: "flex",
+                  gap: "0.75rem",
+                  justifyContent: "center",
+                }}
+              >
+                <NavLink to={`/hangszer/${id}`} className="btn btn-primary">
+                  Mégse
+                </NavLink>
+
+                <button type="submit" className="btn btn-danger">
+                  Törlés
+                </button>
+              </div>
+            </form>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
